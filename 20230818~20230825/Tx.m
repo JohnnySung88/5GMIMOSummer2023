233b2cc06dd4a5f63a6420b70836f4ff4096697e
Kiri Sung <johnnysung28@gmail.com>
2023-08-21 16:21:31 +0800

    Tx

--- 20230818~20230825/Tx.m
+++ 20230818~20230825/Tx.m
@@ -1,27 +1,23 @@
+load('LDPC_11nD2_1296b_R12.mat');
+Pict_origin  = imread('Cat.jpg');
+Pict_origin  = imresize(Pict_origin,[384,384]);
+Pict_row	 = size(Pict_origin,1);
+Pict_col	 = size(Pict_origin,2);
+Pict_bin	 = [dec2bin(Pict_origin(:,:,1));
+				dec2bin(Pict_origin(:,:,2));
+				dec2bin(Pict_origin(:,:,3))];
+Pict_size	 = size(Pict_bin,1);
+Pict_bin_RE	 = [Pict_bin;zeros(648-mod(Pict_row*Pict_col*3,648),8)];
+Pict_bin_RE	 = reshape(Pict_bin_RE,[],648);
+encode 		 = mod(Pict_bin_RE * double(LDPC.G.x),2);
+
 SNR = 10^( SNR_in_dB/10);
 No  = 10^(-SNR_in_dB/10);
-
-%LDPC數據生成  totle 1772232(扣除DMRS、PSS) 5469*1296+1104
-%load('LDPC_11nD2_1296b_R12.mat');
-
-figure_origin = imread("Cat.jpg");
-figure_origin = imresize(figure_origin, [384,384]);
-figure_row	  = size(figure_origin,1);
-figure_col	  = size(figure_origin,2);
-figure_bin1	  = dec2bin(figure_origin(:,:,1));
-figure_bin2	  = dec2bin(figure_origin(:,:,2));
-figure_bin3	  = dec2bin(figure_origin(:,:,3));
-figure_bin	  = [figure_bin1,figure_bin2,figure_bin3];
-figure_size	 = size(figure_bin,1);
-figure_bin_RE	 = [figure_bin;zeros(648-mod(figure_row*figure_col*3,648),8)];
-figure_bin_RE	 = reshape(figure_bin_RE,[],648);
-
-encode 		 = mod(figure_bin_RE * double(LDPC.G.x),2);
 q_bit        = 4;
 LDPC_num	 = size(encode,1);
 LDPC_bin 	= reshape(encode,q_bit ,[]);%資料順序不論(準備進調變)
 LDPC_dec_L	= sum(LDPC_bin.'.*binTable,2);%bin2dec
-LDPC_mod_L	= QAMMOD (LDPC_dec_L,  QAM)*NF;
+LDPC_mod_L	= qammod (LDPC_dec_L,  QAM)*NF;
 LDPC_mod	= zeros(1644,560,Tx);
 LDPC_mod(DATA_Pos) = [ LDPC_mod_L ; zeros( length(DATA_Pos)-length(LDPC_mod_L) ,1)];
 LDPC_mod(DMRS1_Pos) = DMRS_DATA;
