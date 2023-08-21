3c5a63d88856ac050d16b67de067aa887cf7dd1a
Kiri Sung <103089663+JohnnySung88@users.noreply.github.com>
2023-08-21 15:31:10 +0800

    Move

--- 20230818~20230825/Tx.m
+++ 20230818~20230825/Tx.m
@@ -4,7 +4,7 @@ No  = 10^(-SNR_in_dB/10);
 %LDPC數據生成  totle 1772232(扣除DMRS、PSS) 5469*1296+1104
 %load('LDPC_11nD2_1296b_R12.mat');
 
-figure_origin = imread("Figure.png");
+figure_origin = imread("Cat.jpg");
 figure_origin = imresize(figure_origin, [384,384]);
 figure_row	  = size(figure_origin,1);
 figure_col	  = size(figure_origin,2);
@@ -45,20 +45,4 @@ for symbol=1:14*4*10
 		index = index+2048+208;
 	end
 end
-%通道
-PowerdB 	= [ -2 -8 -10 -12 -15 -18].';
-PowerdB_MIMO= repmat(PowerdB,1,Rx,Tx);
-H_Channel 	= sqrt(10.^(PowerdB_MIMO./10)).* sqrt( 1/ Tx );
-Ntap		= length(PowerdB);
-H_Channel   = H_Channel .* ( sqrt( 1/2 ) .* ( randn(Ntap,Rx,Tx) + 1i*randn(Ntap,Rx,Tx) ) );
-%捲積
-pure_y		= zeros(Rx,1228800+5);
-for Tx_n = 1:Tx
-	for Rx_n = 1:Rx
-		pure_y(Rx_n,:) = pure_y(Rx_n,:) + conv( x_CP(1,:,Tx_n) , H_Channel(:,Rx_n,Tx_n) );
-	end
-end
-pure_y(:,1228801:end) = [];	%刪除最後5筆資料
-%產生訊號
-n 			= sqrt(No/2) *( randn(Tx,1228800) + randn(Tx,1228800)*1i );% randn產生noise variance=No
-Rx_signal	= pure_y + n;
+
