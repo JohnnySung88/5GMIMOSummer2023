26ae0a311e4d09dcf462c22954ccac343a24c84e
Kiri Sung <103089663+JohnnySung88@users.noreply.github.com>
2023-08-21 17:20:55 +0800

    Add LPDC Test

--- 20230818~20230825/Test.m
+++ 20230818~20230825/Test.m
@@ -4,3 +4,4 @@ Rx_signal = simChannel(Tx_signal,20);
 DTinfo='LMMSE'
 CFO_ingore = 5;
 [Time,Biterror,Capacity_sum,Rx1_SNR,Rx2_SNR,Lena_RGB] = Rx_fun(frame_data,Rx_signal,DTinfo,CFO_ingore)
+imshow(Lena_RGB);
