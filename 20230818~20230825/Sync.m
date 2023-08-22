5b22d02cfd498f076d5f1b4ded958b4da5df3790
Kiri Sung <johnnysung28@gmail.com>
2023-08-22 16:06:42 +0800

    ABS

--- 20230818~20230825/Sync.m
+++ 20230818~20230825/Sync.m
@@ -3,8 +3,8 @@ function [y_out] = Sync(frame_data,y)
 	thread2 =parfeval(backgroundPool,@xcorr,2,y(2,1:1228800/5),frame_data.PSS_t);
     [corr_1,corr_pos_1] = fetchOutputs(thread1);
     [corr_2,corr_pos_2] = fetchOutputs(thread2);
-	[~,max_pos_1]=max(corr_1);
-	[~,max_pos_2]=max(corr_2);
+	[~,max_pos_1]=max(abs(corr_1));
+	[~,max_pos_2]=max(abs(corr_2));
 	y_pos(1) = corr_pos_1(max_pos_1) - frame_data.syc_pos+1;
 	y_pos(2) = corr_pos_2(max_pos_2) - frame_data.syc_pos+1;
     if( (y_pos(1)+1228800-1 <= length(y)) & (y_pos(2)+1228800-1 <= length(y)) )
