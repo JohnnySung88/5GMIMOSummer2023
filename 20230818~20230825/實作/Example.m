cf5a8c0c2ac0ddda7a38b5a621c88393558fde27
Kiri Sung <103089663+JohnnySung88@users.noreply.github.com>
2023-08-23 15:43:29 +0800

    Update Example

--- 20230818~20230825/實作/Example.m
+++ 20230818~20230825/實作/Example.m
@@ -7,8 +7,8 @@ function [y_out] = Sync(frame_data,y)
     [corr_2,corr_pos_2] = fetchOutputs(thread2);
 	[~,max_pos_1]=max(abs(corr_1));
 	[~,max_pos_2]=max(abs(corr_2));
-	y_pos(1) = corr_pos_1(max_pos_1) - frame_data.syc_pos+1;
-	y_pos(2) = corr_pos_2(max_pos_2) - frame_data.syc_pos+1;
+	y_pos(1) = corr_pos_1(max_pos_1) - frame_data.syc_pos+1 - 3;
+	y_pos(2) = corr_pos_2(max_pos_2) - frame_data.syc_pos+1 - 3;
     if( (y_pos(1)+1228800-1 <= length(y)) & (y_pos(2)+1228800-1 <= length(y)) )
         y_out    = zeros(2,1228800);
         y_out(1,:) 	  = y(1,y_pos(1):y_pos(1)+1228800-1);
